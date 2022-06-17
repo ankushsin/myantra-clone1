eb58f7ac89f88eb9bf784ab3ed37b9c29c7f5624
ankushsin <101393390+ankushsin@users.noreply.github.com>
2022-06-17 13:09:09 +0530

    1st repo

---
+++ men.js
@@ -0,0 +1,219 @@
+
+let productData =[
+    {
+    brand: "Roadster",
+    name: "Men raw pure edge cotton t-shirt",
+    price: "Rs 149", 
+    productID: "men1",
+    catagory: "Casual",
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/2275352/2021/12/16/160f8c3b-7e70-438d-ae50-0e584a5c34681639647173379-Roadster-Men-Black-Raw-Edge-T-shirt-6791639647172958-1.jpg",
+  },
+  {
+    brand: "Polo Ralph Lauren",
+    name: "Men Stripped polo collor t-shirt",
+    price: "Rs 12059", 
+    productID: "men2",
+    catagory: "festive",
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/2275373/2018/1/24/11516793428292-Roadster-Men-Charcoal-Solid-Round-Neck-T-shirt-3641516793428158-1.jpg",
+  },
+  {
+    brand: "Roadster",
+    name: "Solid round neck t-shirt",
+    price: "Rs 159",  
+    productID: "men3",
+    catagory: "party",
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/17971096/2022/4/21/09dad835-0f41-4283-9e55-d7521d91f7731650533620762PoloRalphLaurenMenNavyBlueStripedPoloCollarT-shirt1.jpg",
+  },
+  {
+    brand: "Justice league",
+    name: "flash print t-shirt",
+    price: "Rs 159", 
+    productID: "men4",
+    catagory: "formal",
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/14533026/2021/7/20/56789a94-77ea-4fb4-84b6-4956777027841626762996285-Justice-League-Men-Tshirts-8141626762995800-1.jpg",
+  },
+  {
+    brand: "Polo Ralph Lauren",
+    name: "Men polo collor T-shirt",
+    price: "Rs 11500",
+    productID: "men5",
+    catagory: "workout",
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/17796248/2022/4/7/392a2638-baf1-4b14-88c2-47a74a0e8afe1649331090021PoloRalphLaurenMenRedPoloCollarAppliqueT-shirt1.jpg",
+  },
+  {
+    brand: "Polo Ralph Lauren",
+    name: "Men polo collor T-shirt",
+    price: "Rs 12375",  
+    productID: "men6",
+    catagory: "travel",
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/16867832/2022/1/18/eb37500e-16dc-4587-bc5e-0b29f5a68fed1642512252209PoloRalphLaurenMenMulticolouredStripedPoloCollarAppliqueT-sh1.jpg",
+  },
+  {
+    brand: "MOSCHINO COUTURE",
+    name: "Men solid polo collar t-shirt",
+    price: "Rs 24000",  
+    productID: "men7",
+    catagory: "sports",
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/17612488/2022/3/22/8e09926e-3415-423d-8034-7428643df99b1647964378443MOSCHINOCOUTUREMenGreenPoloCollarT-shirt1.jpg",
+  },
+  {
+    brand: "Versace jeans couture",
+    name: "Men solid polo collar t-shirt",
+    price: "Rs 27500",  
+    productID: "men8",
+    catagory: "Casual",
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/17802018/2022/4/7/cef52e0b-38a2-49c2-b1ba-3026ecf9f2e01649343630162VersaceJeansCoutureMenBlackPoloCollarDrop-ShoulderSleevesPur1.jpg",
+  },
+  {
+    brand: "ether",
+    name: "Unisex pure cotton t-shirt",
+    price: "Rs 199",
+    productID: "men9",
+    catagory: "festive",
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/14596832/2021/9/28/f9f2fbf1-6ebe-4095-9dfc-f15238b173f11632831449726-ether-Men-Tshirts-3161632831449011-1.jpg",
+  },
+  {
+    brand: "Roadster",
+    name: "Solid round neck t-shirt",
+    price: "Rs 199", 
+    productID: "men10",
+    catagory: "party",
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/1996781/2018/1/9/11515489592906-Roadster-Men-Olive-Green-Solid-Round-Neck-T-shirt-1551515489592784-1.jpg"
+  },
+  {
+    brand: "Kook N Keech",
+    name: "Unisex pure cotton t-shirt",
+    price: "Rs 199 ",
+    productID: "men11",
+    catagory: "formal",
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/14858870/2021/8/19/c82dc1cb-e88d-4c6e-a1d2-d93a35fe78d71629344764309-Kook-N-Keech-Unisex-Red-Printed-Detail-Pure-Cotton-T-shirt-8-1.jpg",
+  },
+  {
+    brand: "Roadster",
+    name: "Solid round neck t-shirt",
+    price: "Rs 159",  
+    productID: "men12",
+    catagory: "workout",
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/2275373/2018/1/24/11516793428292-Roadster-Men-Charcoal-Solid-Round-Neck-T-shirt-3641516793428158-1.jpg",
+  },
+  {
+    brand: "Roadster",
+    name: "Solid round neck t-shirt",
+    price: "Rs 159", 
+    productID: "men13",
+    catagory: "travel",
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/1996736/2018/9/1/085cd032-bf4f-45cb-9601-01365c208fd91535792113650-Roadster-Men-Navy-Solid-Round-Neck-T-shirt-6601535792111856-1.jpg",
+  },
+  {
+    brand: "Roadster",
+    name: "Solid round neck t-shirt",
+    price: "Rs 159",
+    productID: "men14",
+    catagory: "sports",
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/15244916/2021/9/28/e0edb72f-62d0-49a0-bf80-95a75f3d755f1632831889124-ether-Men-Tshirts-1471632831888572-1.jpg",
+  },
+  {
+    brand: "Roadster",
+    name: "Solid round neck t-shirt",
+    price: "Rs 159",
+    productID: "men15",
+    catagory: "Casual",
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/2330209/2018/1/18/11516279524191-Roadster-Men-Black-Solid-V-Neck-T-shirt-3631516279523996-1.jpg",
+  },
+  {
+    brand: "Roadster",
+    name: "Solid round neck t-shirt",
+    price: "Rs 159",
+    productID: "men16",
+    catagory: "festive",
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/2499218/2018/4/14/11523698665779-Roadster-Men-Grey-Printed-Round-Neck-T-shirt-7941523698665511-1.jpg",
+  },
+  {
+    brand: "Roadster",
+    name: "Solid round neck t-shirt",
+    price: "Rs 159",
+    productID: "men17",
+    catagory: "party",
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/2498485/2018/5/4/11525433734244-Roadster-Men-Tshirts-4131525433733991-1.jpg",
+  },
+  {
+    brand: "Roadster",
+    name: "Solid round neck t-shirt",
+    price: "Rs 159",
+    productID: "men18",
+    catagory: "formal",
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/2498514/2018/5/11/e5002c9c-5431-49b2-915d-034736be10b71526031613865-Roadster-Men-Tshirts-3601526031613685-1.jpg",
+  },
+  {
+    brand: "Roadster",
+    name: "Solid round neck t-shirt",
+    price: "Rs 159",
+    productID: "men19",
+    catagory: "workout",
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/11632476/2020/7/15/3e39f6d7-e7fb-4cc7-958e-65cbcdf88dc01594808343802-Roadster-Men-Tshirts-281594808342107-1.jpg",
+  },
+  {
+    brand: "Roadster",
+    name: "Solid round neck t-shirt",
+    price: "Rs 159",
+    productID: "men20",
+    catagory: "travel",
+    img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/9930081/2019/8/5/4ab39d3c-9a84-4b73-b4b3-0760ebdc064d1565005834172-HIGHLANDER-Men-Navy-Blue-Solid-Round-Neck-T-shirt-6401565005-1.jpg",
+  },
+  
+]
+let productsCont = document.getElementById("main2");
+let filter = document.getElementById("filter");
+
+
+function displayProducts(productData){
+    productsCont.innerHTML="";
+    productData.forEach(function(element){
+        let product = document.createElement("div");
+        
+        
+        
+       
+        let img = document.createElement("img");
+        img.src= element.img;
+
+        let name = document.createElement("h5");
+        name.innerText = element.name;
+
+        let brand = document.createElement("p");
+        brand.innerText = element.brand;
+
+        let catagory = document.createElement("h4");
+        catagory.innerText = element.catagory;
+
+        let productID = document.createElement("h4");
+        productID.innerText = element.productID;
+
+        let price = document.createElement("h3");
+        price.innerText = element.price;
+        
+
+        let addtocart = document.createElement("button");
+        addtocart.innerText = "Add To Cart";
+       
+
+        product.append(img,name,brand,catagory,price,addtocart);
+        productsCont.append(product);
+    })
+  
+}
+  displayProducts(productData);
+  
+  filter.addEventListener("change",function(){
+    if(filter.value === "All"){
+      displayProducts(productData);
+    }else{
+      let filtered = productData.filter(function(element){
+        return element.catagory===filter.value
+      })
+      displayProducts(filtered);
+    }
+  })
+ 
+
+    
